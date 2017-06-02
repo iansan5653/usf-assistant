b1df52548b3bf36cfb1d861d765092601b308a6c
Ian Sanders <iansan5653@gmail.com>
2017-06-01 21:14:58 -0400

    more testing

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var asst = require('./asst/index');
+
 var app = express();
 
 // view engine setup
@@ -24,6 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+app.use('/asst', asst)
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.setHeader('Content-Type', 'application/json');
+  response = JSON.stringify({ a: 1 });
+  res.send(response);
 });
 
 module.exports = router;
