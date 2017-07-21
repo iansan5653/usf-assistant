b8245aa109cd04339cf85f30e40a200da97b9c8a
Ian Sanders <iansan5653@gmail.com>
2017-07-21 09:44:24 -0400

    Testing..

--- comms/index.js
+++ comms/index.js
@@ -186,9 +186,9 @@ module.exports.apiai = function(req, res, data) {
   // Input context possibly includes route
   function closestStop(app) {
   	//var context = assistant.getContext('request_permission');
-  	var granted = app.isPermissionGranted();
-  	console.log(granted);
   	app.tell('Cool, thanks. You\'re at ');
+  	console.log('HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH');
+  	console.log(app.getDeviceLocation());
   	/*
   	// Testing if null includes if the location couldn't be found and permissions were granted
 		if (app.getDeviceLocation() !== null) {
