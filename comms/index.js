f2ef9d9879beabb1216e4d43cf21826b374b9f3e
Ian Sanders <iansan5653@gmail.com>
2017-07-21 10:22:22 -0400

    Cleanup

--- comms/index.js
+++ comms/index.js
@@ -19,18 +19,6 @@ function getDistance(cachedStop, currentLoc) {
 	return distance;
 }
 
-from math import radians, cos, sin, asin, sqrt
-def haversine(lon1, lat1, lon2, lat2):
-    # convert decimal degrees to radians 
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    # haversine formula 
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
-    km = 6367 * c
-    return km
-
 module.exports.apiai = function(req, res, data) {
 	var app = new ApiAiApp({request: req, response: res});
 	var hasScreen =
