5e0abfdf8951796e1d2c1495abec2889777bf9a8
Ian Sanders <iansan5653@gmail.com>
2017-08-11 11:38:08 -0400

    Completely rewrote nextBus function to be clearer, more efficient, and more comprehensive of every situation.

--- comms/index.js
+++ comms/index.js
@@ -269,24 +269,19 @@ module.exports.apiai = function(req, res, data) {
   }
 
   // Next bus(es) at a stop
-  // Input context show_all simply indicates that this function was called within the last minute and the user is asking about all routes
+  // Input argument show_all indicates that the user is asking about all routes
   // Input context selected_route indicated information will only be provided about a previously discussed route
   // Input context selected_stop waives the requirement for a stop parameter and uses a previously discussed stop
   function nextBus(app) {
-  	var routeContext = app.getContext('selected_route');
-  	var routeArg = app.getArgument('route');
-  	var stopContext = app.getContext('selected_stop');
-  	console.log(stopContext);
-  	var stopArg = app.getArgument('stop');
-
-  	// If we provide one route's info and the user explicitly wants them all, give it to them as a followup
-  	var showAll = app.getArgument('show_all');
+  	var routeContext = app.getContext('selected_route'),
+  			routeArg = app.getArgument('route'),
+  			stopContext = app.getContext('selected_stop'),
+  			stopArg = app.getArgument('stop'),
+  			showAll = app.getArgument('show_all');
 
   	var stop = null;
-  	// If there's no stop context, then an explicit stop argument is required so stop should never be null
-  	if(stopArg) {
+   	if(stopArg) {
   		stop = data.stops.find(stopObject => stopObject.Name == stopArg);
-  		app.setContext('selected_stop', 3, stop);
   	} else if(stopContext) {
   		stop = stopContext.parameters;
   	}
@@ -296,123 +291,164 @@ module.exports.apiai = function(req, res, data) {
   	// If a route is explicitly provided, use it and set the context
   	if(routeArg) {
 	  	route = data.routes.find(routeObject => routeObject.Letter == routeArg);
-	  	app.setContext('selected_route', 3, route);
+	  	//It's possible that the route argument doesn't correspond to a route:
+	  	if(route) {
+	  		app.setContext('selected_route', 3, route);
+	  	}
 	  } else if(routeContext) {
 	  	route = routeContext.parameters;
+	  	app.setContext('selected_route', 3, route);
 	  }
 
 	  var response = app.buildRichResponse();
 
-  	if(stop) {
-  		request('https://usfbullrunner.com/Stop/' + stop.ID + '/Arrivals?customerID=3', 
-			(error, res1, body) => {
-
-				bodyJSON = JSON.parse(body);
-
-				if (!error && res1.statusCode == 200) {
-					if (bodyJSON.length === 0) {
-						response
-							.addSimpleResponse('There aren\'t any buses servicing ' + stop.Name + ' (Stop ' + stop.Number + ') right now. Please ensure that that route is currently operating.')
-							.addSuggestions(['Are the buses running?'])
-							.addSuggestionLink('Bull Runner hours', 'http://www.usf.edu/administrative-services/parking/transportation/hours-of-operation.aspx');
-						app.ask(response);
-
-					} else if(bodyJSON.length === 1 && showAll) {
-						var routeName = data.routes.find(routeObject => routeObject.ID == route.ID).Name;
-
-						// {Route A} is the only route that connects with that stop.
-						response.addSimpleResponse(routeName + ' is the only route that connects with that stop.')
-							.addSuggestions(['Status of this route']);
-
-					} else if(bodyJSON.length === 1 || (routeArg) || (routeContext && !showAll)) {	
-						// If only one route is servicing the stop, or if there is a route given, or if there is a route context and NOT a show all context
-
-						// Use the first (only) route if no route given, otherwise use the given route:
-						var index = (route) ? bodyJSON.findIndex(routeObject => routeObject.RouteID == route.ID) : 0;
-
-						var routeName = data.routes.find(routeObject => routeObject.ID == route.ID).Name;
-
-						if(index != -1) {
-							var seconds = bodyJSON[index].Arrivals[0].SecondsToArrival;
-							var minutes = Math.floor(seconds / 60);
-							
-							// Sometimes, if a bus is already there the minutes will be 0 or negative. In that case we use the next bus if there is one.
-							if (minutes < 1 && bodyJSON[index].Arrivals.length > 1) {
-								seconds = bodyJSON[index].Arrivals[1].SecondsToArrival;
-								minutes = Math.floor(seconds / 60);
-							} else if (minutes < 0) {
-								// Even if there are no other buses on the route, it never makes since to say a bus will arrive in -N minutes
-								minutes = 0;
-							}
-
-							var plural = (minutes == 1) ? '' : 's'; // If multiple minutes, use plural units
-
-							if(!route) {
-								// THe next bus will arrive on {Route A} in {10} minute{s}.
-								response.addSimpleResponse('The next bus will arrive on ' + routeName + ' in ' + minutes + ' minute' + plural + '.')
-									.addSuggestionLink('navigation', getNavURL(stop))
-									.addSuggestions(['Status of this route']);
-							} else {
-								// This phrasing makes it clear that this is more specific
-								// The next bus on {Route A} will arrive at {Communication Sciences} ({Stop 222}) in {10} minute{s}.
-								response.addSimpleResponse('The next bus on ' + routeName + ' will arrive at ' + stop.Name + ' (Stop ' + stop.Number + ') in ' + minutes + ' minute' + plural + '.')
-									.addSuggestionLink('navigation', getNavURL(stop))
-									.addSuggestions(['What about other routes?', 'Status of this route']);
-							}
-
-						} else {
-							// {Route A} isn't currently servicing {Communication Sciences} ({Stop 222}). This is probably because the route isn't active or doesn't connect to that stop.
-							response.addSimpleResponse(routeName + ' isn\'t currently servicing ' + stop.Name + ' (Stop ' + stop.Number + '). This is probably because the route isn\'t active or doesn\'t connect to this stop.')
-								.addSuggestions(['What about other routes?', 'Are the buses running?', 'Status of this route'])
-								.addSuggestionLink('Bull Runner hours', 'http://www.usf.edu/administrative-services/parking/transportation/hours-of-operation.aspx');
-						}
-
-					} else {
-						var strings = ['There are ' + bodyJSON.length + ' routes serving this stop right now.'];
-
-						// For each route, find the time and add it to the strings
-						bodyJSON.forEach(stopRoute => {
-							var seconds = stopRoute.Arrivals[0].SecondsToArrival;
-							var minutes = Math.floor(seconds / 60);
-
-							// Sometimes, if a bus is already there the minutes will be 0 or negative. In that case we use the next bus if there is one.
-							if(minutes < 1 && stopRoute.Arrivals.length > 1) {
-								seconds = stopRoute.Arrivals[1].SecondsToArrival;
-								minutes = Math.floor(seconds / 60);
-							}
-
-							var plural = (minutes == 1) ? '' : 's'; // If multiple minutes, use plural units
-
-							var routeName = data.routes.find(routeObject => routeObject.ID == stopRoute.RouteID).Name;
-
-							strings.push('On ' + routeName + ', the next bus will arrive in ' + minutes + ' minute' + plural + '.');
-						});
-
-						// Add a transition phrase to the last string if it's a longer list
-						if(strings.length > 2) {
-							strings[strings.length - 1] = strings[strings.length - 1].replace(/^\S+/g, 'Finally, on');
-						}
-
-						response.addSimpleResponse(strings.join(' '))
-							.addSuggestionLink('navigation', getNavURL(stop));
-
-					}
-
-					app.ask(response);
-
-				} else {
-					app.tell('Sorry, there was an error retrieving information from the Bull Runner. Please try again later.');
-				}
-			});
-
-  	} else {
-  		let response = app.buildRichResponse()
-  			.addSimpleResponse('I can\'t tell which stop you would like information about. It may help to refer to the stop by its number rather than its name.')
+	  if(!stop) {
+	  	// If a stop isn't specified by context or by argument
+  		response
+  			.addSimpleResponse('I can\'t tell which stop you would like to know about. It may help to refer to the stop by its number rather than its name.')
   			.addSuggestions(['What is the closest stop?', 'Are any buses running?', 'Stop ' + data.stops.random().Number]);
   		app.ask(response);
-  	}
+
+	  } else {
+	  	app.setContext('selected_stop', 3, stop);
+	  	// If a stop is specified, set its context even if the rest of the function doesn't work
+
+	  	// Build an array of routes serving the stop
+	  	stop.Routes.letters = [];
+	  	stop.Routes.forEach(routeID => stop.Routes.letters.push(data.routes.find(routeObject => routeObject.ID == routeID).Letter));
+
+	  	if(routeArg && !route) {
+		  	// A route is not required, but if they do ask for a specific one and it isn't defined that's a problem
+		  	response
+	  			.addSimpleResponse('I can\'t tell which route you\'re asking about. Please try again or ask about other routes.')
+	  			.addSuggestions(['What about other routes?', 'What is the closest stop?', 'What about Route ' + data.routes.random().Letter + '?']);
+	  		app.ask(response);
+
+		  } else if(routeArg && !stop.Routes.includes(route.ID)) {
+		  	// A route was specifically asked for but that route doesn't serve this stop
+		  	response
+	  			.addSimpleResponse('The route you asked about doesn\'t connect with this stop. Try asking about other routes or a different stop.')
+	  			.addSuggestions(['What about other routes?', 'Closest stop on route']);
+	  		app.ask(response);
+
+	  	} else {
+		  	// Now we know that a stop is provided and that either a route is provided or not specified
+		  	request('https://usfbullrunner.com/Stop/' + stop.ID + '/Arrivals?customerID=3', (error, res, body) => {
+					if (error || res.statusCode !== 200) {
+						// Request failed
+						app.tell('Sorry, there was a problem retrieving information from the Bull Runner server. Please try again later.');
+						console.error(error);
+						console.info(res);
+
+					} else {
+
+						var routes = null;
+						// There's always a chance the response is malformed:
+						try {
+							routes = JSON.parse(body);
+						} catch(err) {
+							console.error(err);
+						}
+
+						if(!routes) {
+							app.tell('Sorry, there was an issue processing information from the Bull Runner server. Please try again later.');
+
+						} else {
+							if (route) route.Arrivals = routes.find(routeObject => route.ID == routeObject.RouteID);
+							// Will be falsy if the route asked for is not one of the active routes
+
+							// {Marshall Student Center} (Stop {401})
+							var stopPhrase = stop.Name + '(Stop ' + stop.Number + ')';
+
+							if(routeArg && !route.Arrivals) {
+								// A route was specifically asked for, but it isn't running right now
+								response
+									.addSimpleResponse('That route isn\'t serving ' + stopPhrase + ' right now. Please check the Bull Runner hours of operation.')
+									.addSuggestions(['Are the buses running?', 'Status of Route ' + route.Letter])
+									.addSuggestionLink('Bull Runner hours', 'http://www.usf.edu/administrative-services/parking/transportation/hours-of-operation.aspx');
+							} else if(routes.length === 0) {
+								// Stop is inactive
+								response
+									.addSimpleResponse('There aren\'t any buses serving ' + stopPhrase + ' right now. Please check the Bull Runner hours of operation.')
+									.addSuggestions(['Are the buses running?', 'Status of Route ' + stop.Routes.letters.random()])
+									.addSuggestionLink('Bull Runner hours', 'http://www.usf.edu/administrative-services/parking/transportation/hours-of-operation.aspx');
+
+							} else if(routes.length == 1 || (!showAll && (routeArg || (routeContext && route.Arrivals)))) {
+								// This is a stop on one route, not a connecting stop
+								// OR (the user did NOT specify all routes AND (either a route argument
+								// OR (a *valid* route context is provided)))
+
+								// This will trigger if there is an invalid route context and only one route, which is fine
+								// because contexts can be safely ignored if they are apparently incorrect
+
+								// Overwrite route, since there's only one possible route
+								if(routes.length == 1) route = data.routes.find(routeObject => routes[0].routeID == routeObject.ID);
+								app.setContext('selected_route', 3, route);
+
+								// Time until the next bus, in minutes
+								var time = Math.floor(routes[0].Arrivals[0].SecondsToArrival / 60);
+								// Sometimes, if a bus is already at the stop the minutes will be 0 or negative. In that case we use the next bus if there is one.
+								if (time < 1 && routes[0].Arrivals.length > 1) {
+									time = Math.floor(routes[0].Arrivals[1].SecondsToArrival / 60);
+								}
+								if (time < 0) time = 0;
+
+								time.s = (time == 1) ? '' : 's'; // If multiple or 0 minutes, use plural
+
+								response
+									.addSuggestions(['Status of this route', 'Closest stop'])
+									.addSuggestionLink('nagivation', getNavURL(closest));
+
+
+								if(showAll) {
+									response.addSimpleResponse('The only route servicing this stop right now is ' + route.Name + '. The next bus on this route will arrive in ' + time + ' minute' + time.s + '.')
+								} else if(routeArg || (routeContext && route.Arrivals)) {
+									response.addSimpleResponse('The next bus on ' + route.Name + ' will arrive in ' + time + ' minute' + time.s + '.');
+								} else {
+									response.addSimpleResponse('The next bus will arrive on ' + route.Name + ' in ' + time + ' minute' + time.s + '.');
+								}
+
+							} else {
+								// There are multiple routes going to this stop (it's a connecting stop) and we want to talk about all of them
+								var strings = ['There are ' + routes.length + ' routes serving this stop right now.'];
+
+								// For each route, find the time and add it to the strings
+								routes.forEach(stopRoute => {
+									//TIme until the next bus, in minutes
+									var time = Math.floor(stopRoute.Arrivals[0].SecondsToArrival / 60);
+									// Sometimes, if a bus is already there the minutes will be 0 or negative. In that case we use the next bus if there is one.
+									if(time < 1 && stopRoute.Arrivals.length > 1) {
+										time = Math.floor(stopRoute.Arrivals[1].SecondsToArrival / 60);
+									}
+									if(time < 0) time = 0;
+
+									time.s = (time == 1) ? '' : 's'; // If multiple or 0 minutes, use plural units
+
+									var routeName = data.routes.find(routeObject => routeObject.ID == stopRoute.RouteID).Name;
+
+									strings.push('On ' + routeName + ', the next bus will arrive in ' + minutes + ' minute' + time.s + '.');
+								});
+
+								// Add a transition phrase to the last string if it's a longer list
+								if(strings.length > 2) {
+									strings[strings.length - 1] = strings[strings.length - 1].replace(/^\S+/g, 'Finally, on');
+								}
+
+								response.addSimpleResponse(strings.join(' '))
+									.addSuggestions(['Overall system status', 'Closest stop'])
+									.addSuggestionLink('navigation', getNavURL(stop));
+
+							}
+
+							app.ask(response);
+						}
+					} 
+			  });
+			}
+		}
   }
 
+  // Fallback for when we just don't get it
   function fallback(app) {
   	var hasScreen = app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT);
   	var simpleResponse = 'Sorry, I don\'t understand. Try asking about the status of the system, a specific stop, or a specific route.';
